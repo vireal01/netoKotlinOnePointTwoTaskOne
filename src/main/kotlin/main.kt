ef6a232be810df05e29f13c2850f14a29b2c2b4f
vireal01 <vireal014@yandex.ru>
2022-02-09 02:57:58 +0300

    completed task2 of lesson 3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,26 @@
 fun main() {
-    val moneyToPay = 5420000
-    val tax = if(moneyToPay * 0.0075 > 3500) (moneyToPay * 0.0075).toFloat() else 3500F
-    val amount = moneyToPay + tax
-    println("сумма перевода: ${(amount / 100).toInt()} рублей ${(amount % 100).toInt()} копеек")
+    fun getVisaOrMirCommission(moneyToPay: Int):Int {
+       val tax = if(moneyToPay * 0.0075 > 3500) (moneyToPay * 0.0075).toFloat() else 3500F
+        return tax.toInt()
+    }
+
+    fun getMasterOrMaestroCommission(moneyToPay: Int, sumOfPreviousTransactions: Int ):Int {
+        val tax = if(moneyToPay + sumOfPreviousTransactions > 7_500_000 ) (moneyToPay * 0.006 + 2_000) else 0
+        return tax.toInt()
+    }
+
+    fun outputPrettier(amount: Int): String{
+        return "сумма транзакции ${amount / 100} руб. ${amount % 100} коп."
+    }
+
+    fun getSumOfTransaction(moneyToPay: Int, sumOfPreviousTransactions: Int = 0, cardHolder: String = "VK"):String {
+        val commissionAmount = when(cardHolder){
+            "Maestro", "Mastercard" -> getMasterOrMaestroCommission(moneyToPay, sumOfPreviousTransactions )
+            "Visa", "Mir" -> getVisaOrMirCommission(moneyToPay )
+            else -> 0
+        }
+        return outputPrettier(commissionAmount + moneyToPay)
+    }
+
+    println(getSumOfTransaction(moneyToPay = 400_000, sumOfPreviousTransactions = 7200000))
 }
